05afb94227257288c1a61df6f3b8d2a39d9c35dd
tylerpotts <tyler.potts@dtn.com>
2023-08-03 09:18:01 -0500

    make conda install less verbose, still time it

--- metaflow/plugins/conda/batch_bootstrap.py
+++ metaflow/plugins/conda/batch_bootstrap.py
@@ -87,12 +87,18 @@ def download_conda_packages(flow_name, env_id, datastore_type):
 def install_conda_environment(env_id, packages):
     args = [
         "if ! type conda  >/dev/null 2>&1; \
+        echo starting miniforge.sh download; date; \
             then wget --no-check-certificate https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh -O Miniforge3.sh >/dev/null 2>&1; \
-            bash ./Miniforge3.sh -b >/dev/null 2>&1; export PATH=$PATH:$HOME/miniforge3/bin; fi",
+        echo got miniforge.sh file; date; \
+            bash ./Miniforge3.sh -b >/dev/null 2>&1; \
+        echo finished miniforge.sh installation; date; \
+        export PATH=$PATH:$HOME/miniforge3/bin; fi",
         "cd {0}".format(os.path.join(os.getcwd(), "pkgs")),
+        "echo starting conda create; date;",
         "conda create --yes --no-default-packages -p {0} --no-deps {1} >/dev/null 2>&1".format(
             os.path.join(os.getcwd(), env_id), " ".join(packages)
         ),
+        "echo finished conda create; date;",
         "cd {0}".format(os.getcwd()),
     ]
     if ENV_ESCAPE_PY is not None:
